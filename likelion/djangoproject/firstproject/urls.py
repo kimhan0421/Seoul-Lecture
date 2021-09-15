e54277defed445b2520540a3fb6d95df070cac87
hansol <kimhan0421@naver.com>
2021-09-15 22:01:15 +0900

    Feat: djangoproject folder split

--- likelion/djangoproject/firstproject/urls.py
+++ likelion/djangoproject/firstproject/urls.py
@@ -20,4 +20,5 @@ import wordcount.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wordcount.views.home, name='home'),
+    path('about/', wordcount.views.about, name='about'),
 ]
